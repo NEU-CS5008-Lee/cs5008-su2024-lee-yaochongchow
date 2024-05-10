c96c2114449a889cad9e4303bf832ff7df5b636e
Yao Chong Chow <137484508+yaochongchow@users.noreply.github.com>
2024-05-10 11:07:42 -0700

    Update helloworld.c

--- assignment1/helloworld.c
+++ assignment1/helloworld.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
-// name: Jeongkyu Lee 
-// email: jeo.lee@northeastern.edu
+// name: Yao Chong Chow
+// email: chow.ya@northeastern.edu
 
 int main() {
-  printf("Hello World.  This is [YourName].\n");
-  printf("I am assignment 1 for [This Semester].\n");
+  printf("Hello World.  This is Yao Chong Chow.\n");
+  printf("I am assignment 1 for Summer 2024.\n");
   return 0;
 }
