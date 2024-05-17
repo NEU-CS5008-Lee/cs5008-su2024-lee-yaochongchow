b0088dd4049cd1b7d933f1d56b2fd27fc74ad643
Yao Chong Chow <ycchow@Yaos-MacBook-Pro.local>
2024-05-17 11:28:36 -0700

    Lab Assignment 2

--- lab-assignment2/calculator.c
+++ lab-assignment2/calculator.c
@@ -1,18 +1,40 @@
-//enter your name here
-//enter your email here
+//Yao Chong Chow
+//chow.ya@northeastern.edu
+
 #include<stdio.h>
 int main()
 {
     int a,b,c, choice;
     
     printf("Enter your choice\n");
-    printf(" 1.addition\n 2. Subtraction\n 3. Multiplication\n 4. Division\n");
+    printf(" 1. Addition\n 2. Subtraction\n 3. Multiplication\n 4. Division\n");
     scanf("%d",&choice);
     
-    printf("Enter a and b values");
+    printf("Enter a and b values ");
     scanf("%d %d", &a, &b);
     
-    // Add your code here using switch
+    switch (choice) {
+        case 1:
+            c=a+b;
+            printf("Result: %d", c);
+            break;
+
+        case 2:
+            c=a-b;
+            printf("Result: %d\n", c);
+            break;
+
+        case 3:
+            c=a*b;
+            printf("Result: %d\n", c);
+            break;
+
+        default:
+            c=a/b;
+            printf("Result: %d\n", c);
+            break;
+    }
+
            
     return 0;
 }
--- lab-assignment2/employee.c
+++ lab-assignment2/employee.c
@@ -1,3 +1,6 @@
+//Yao Chong Chow
+//chow.ya@northeastern.edu
+
 /*C Program to read and print the n employee details using structure and dynamic memory allocation*/
 #include <stdio.h>
 #include <stdlib.h>
@@ -10,15 +13,32 @@ typedef struct employee
     char Dept[10];
 
 }employee_t;
+
 /* Read the employee details using scanf*/
 void readData(int n, employee_t* s)
 {
     // Add your code here
+
+    
+    printf("Enter the employeeID, Name, Designation and Department of Employee %d:",n+1);
+    scanf("%d",&s->empId);
+    scanf("%s",s->Name);
+    scanf("%s",s->Designation);
+    scanf("%s",s->Dept);
+    
+
+
 }
 /* Function to print the employee details*/
-void display(int n, employee_t * s)
+void display(int n, employee_t* s)
 {
-    // Add your code here
+    
+    printf("The details of Employee %d :\n",n);
+    printf("empId: %d\n ", s->empId);
+    printf("Name: %s\n ", s->Name);
+    printf("Designation: %s\n ", s->Designation);
+    printf("Dept: %s\n ", s->Dept);
+    printf("\n");
     
 }
 
@@ -28,7 +48,28 @@ int main()
 {
     // Main Function to print the employee details
     // Add your code here
-    
+
+
+        int i;
+        int n;
+
+        printf("Enter the number of employees: ");
+        scanf("%d",&n);
+
+        employee_t *employee_company = (employee_t*)malloc(n * sizeof(employee_t));
+
+        for (i = 0; i < n; ++i)
+        {
+        readData(i, &employee_company[i]);
+        }
+
+        for (i = 0; i < n; ++i)
+        {
+        display(i, &employee_company[i]);
+        }
+
+        free(employee_company);
+        return 0;
 
 }
 
